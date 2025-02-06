00025c4fe96f05f0a67e0f57a7f5e105d2a2f81a
Rimon <melamed.rimon@gmail.com>
2025-02-06 11:05:56 -0500

    fix prompt template for llama, many QOL improvements

--- evil_twins/prompt_optim.py
+++ evil_twins/prompt_optim.py
@@ -45,8 +45,8 @@ PROMPT_TEMPLATES = {
     "suffix": "",
   },
   "phi": {
-    "prefix": "",  # "Instruction: ",
-    "suffix": "",  # "\nOutput: ",
+    "prefix": "",
+    "suffix": "",
   },
   "pythia": {
     "prefix": "",
@@ -72,8 +72,12 @@ PROMPT_TEMPLATES = {
     "prefix": "<start_of_turn>user\n",
     "suffix": "<end_of_turn>\n<start_of_turn>model\n",
   },
+  # "llama-3-instruct": {
+  #   "prefix": "<|start_header_id|>user<|end_header_id|>\n\n",
+  #   "suffix": "<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+  # },
   "llama-3-instruct": {
-    "prefix": "<|start_header_id|>user<|end_header_id|>\n\n",
+    "prefix": "<|start_header_id|>system<|end_header_id|>\n\nCutting Knowledge Date: December 2023\nToday Date: 26 Jul 2024\n\n<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n",
     "suffix": "<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
   },
   "llama-base": {
@@ -127,7 +131,8 @@ MODEL_NAME_OR_PATH_TO_NAME = {
   "google/gemma-1.1-2b-it": "gemma",
   "meta-llama/Meta-Llama-3-8B-Instruct": "llama-3-instruct",
   "meta-llama/Llama-3.1-8B-Instruct": "llama-3-instruct",
-  "meta-llama/Meta-Llama-3-8B": "llama-base",
+  "meta-llama/Llama-3.2-3B-Instruct": "llama-3-instruct",
+  "meta-llama/Llama-3.2-1B-Instruct": "llama-3-instruct",
   "google/gemma-2-2b-it": "gemma-2-it",
   "google/gemma-2-9b-it": "gemma-2-it",
   "HuggingFaceTB/SmolLM2-135M-Instruct": "smollm-2-instruct",
@@ -194,8 +199,12 @@ def build_prompt(
   cur_prompt = PROMPT_TEMPLATES[model_name]["prefix"]
 
   prompt_start_idx = max(len(tokenizer.encode(cur_prompt)) - 1, 0)
-  # account for models that add BOS token
-  if tokenizer.encode(cur_prompt)[0] == tokenizer.bos_token_id:
+  # account for models that add BOS token (account for models that dont have BOS but do have <|im_start|>)
+  if tokenizer.encode(cur_prompt)[0] == tokenizer.bos_token_id or (
+    tokenizer.bos_token_id is None
+    and tokenizer.decode(tokenizer.encode(cur_prompt)[0])
+    in tokenizer.special_tokens_map["additional_special_tokens"]
+  ):
     prompt_start_idx += 1
 
   cur_prompt += prompt
@@ -303,6 +312,8 @@ class DocDataset(Dataset):
     doc_len: int,
     gen_batch_size: int = 10,
     validate_prompt: bool = True,
+    gen_train_docs: bool = True,
+    gen_dev_docs: bool = True,
   ) -> None:
     if isinstance(orig_prompt, str):
       self.orig_wrapped_prompt, self.orig_prompt_slice = build_prompt(
@@ -332,8 +343,18 @@ class DocDataset(Dataset):
       self.wrapped_prompt.shape[-1] + doc_len,
     )
 
-    self.train_docs = self._gen_docs(model, n_docs, doc_len, gen_batch_size)
-    self.dev_docs = self._gen_docs(model, n_docs, doc_len, gen_batch_size)
+    if not gen_train_docs and not gen_dev_docs:
+      print("WARNING: specified NO docs to be generated; continue at your own risk.")
+
+    if gen_train_docs:
+      self.train_docs = self._gen_docs(model, n_docs, doc_len, gen_batch_size)
+    else:
+      self.train_docs = None
+
+    if gen_dev_docs:
+      self.dev_docs = self._gen_docs(model, n_docs, doc_len, gen_batch_size)
+    else:
+      self.dev_docs = None
 
   def _gen_docs(
     self, model: PreTrainedModel, n_docs: int, doc_len: int, gen_batch_size: int
@@ -350,62 +371,94 @@ class DocDataset(Dataset):
     Returns:
       doc tokens `(n_docs, doc_len)`
     """
-    attn_mask = torch.ones_like(self.orig_wrapped_prompt).to(model.device)
     docs = torch.zeros((n_docs, doc_len), dtype=torch.long, device=model.device)
 
     for i in range(0, n_docs, gen_batch_size):
       cur_bsz = min(gen_batch_size, n_docs - i)
 
       # Generate docs w.r.t the original prompt
-      doc_ids = model.generate(
-        self.orig_wrapped_prompt,
-        do_sample=True,
-        top_k=0,
-        top_p=1.0,
-        temperature=1.0,
-        min_new_tokens=doc_len,
-        max_new_tokens=doc_len,
-        num_return_sequences=cur_bsz,
-        attention_mask=attn_mask,
-      )
-
-      # Without using HF generate method
-      # doc_ids = torch.zeros((cur_bsz, doc_len), dtype=torch.long, device=model.device)
-      # cur_prompt = (
-      #   repeat(self.orig_wrapped_prompt, "b k -> (repeat b) k", repeat=cur_bsz)
-      #   .clone()
-      #   .to(model.device)
+      # attn_mask = torch.ones_like(self.orig_wrapped_prompt).to(model.device)
+      # doc_ids = model.generate(
+      #   self.orig_wrapped_prompt,
+      #   do_sample=True,
+      #   top_k=0,
+      #   top_p=1.0,
+      #   temperature=1.0,
+      #   min_new_tokens=doc_len,
+      #   max_new_tokens=doc_len,
+      #   num_return_sequences=cur_bsz,
+      #   attention_mask=attn_mask,
       # )
-      # for j in range(doc_len):
-      #   cur_logits = model(cur_prompt).logits
-      #   cur_logits = cur_logits[:, -1, :]
-      #   cur_logits[..., model.config.bos_token_id] = -float("inf")
-      #   cur_logits[..., model.config.eos_token_id] = -float("inf")
-      #   cur_probs = F.softmax(cur_logits, dim=-1)
-      #   cur_tok = torch.multinomial(cur_probs, 1)
-      #   doc_ids[:, j] = rearrange(cur_tok, "k b -> b k")
-      # self.docs[i : i + cur_bsz] = doc_ids
 
-      docs[i : i + cur_bsz] = doc_ids[:, self.orig_doc_slice]
+      # Without using HF generate method, for consistency
+      doc_ids = torch.zeros(
+        (cur_bsz, doc_len), dtype=torch.long, device=model.device
+      )
+      cur_prompt = (
+        repeat(self.orig_wrapped_prompt, "b k -> (repeat b) k", repeat=cur_bsz)
+        .clone()
+        .to(model.device)
+      )
+      for j in range(doc_len):
+        cur_logits = model(cur_prompt).logits
+        cur_logits = cur_logits[:, -1, :]
+
+        # handle models with logit softcapping (Gemma)
+        if (
+          hasattr(model.config, "final_logit_softcapping")
+          and model.config.final_logit_softcapping is not None
+        ):
+          cur_logits /= model.config.final_logit_softcapping
+          cur_logits.tanh_()
+          cur_logits *= model.config.final_logit_softcapping
+
+        # cur_logits[..., model.config.bos_token_id] = -float("inf")
+        # since we're forcing generation up to doc_len
+        cur_logits[..., model.config.eos_token_id] = -float("inf")
+
+        cur_probs = F.softmax(cur_logits, dim=-1)
+        cur_toks = torch.multinomial(cur_probs, 1)
+        doc_ids[:, j] = rearrange(cur_toks, "b 1 -> b")
+        cur_prompt = torch.cat([cur_prompt, cur_toks], dim=-1)
+
+      docs[i : i + cur_bsz] = doc_ids
 
     return docs
 
   def __len__(self) -> int:
-    return self.train_docs.shape[0]
+    if self.train_docs is not None:
+      return self.train_docs.shape[0]
+    if self.dev_docs is not None:
+      return self.dev_docs.shape[0]
+
+    raise ValueError("No docs generated")
 
   def __getitem__(self, idx: int) -> dict:
-    return {
-      "optim_seq": torch.cat([self.wrapped_prompt[0], self.train_docs[idx]], dim=-1),
-      "orig_seq": torch.cat(
-        [self.orig_wrapped_prompt[0], self.train_docs[idx]], dim=-1
-      ),
-      "optim_seq_dev": torch.cat(
-        [self.wrapped_prompt[0], self.dev_docs[idx]], dim=-1
-      ),
-      "orig_seq_dev": torch.cat(
-        [self.orig_wrapped_prompt[0], self.dev_docs[idx]], dim=-1
-      ),
-    }
+    ret = {}
+    if self.train_docs is not None:
+      ret.update(
+        {
+          "optim_seq": torch.cat(
+            [self.wrapped_prompt[0], self.train_docs[idx]], dim=-1
+          ),
+          "orig_seq": torch.cat(
+            [self.orig_wrapped_prompt[0], self.train_docs[idx]], dim=-1
+          ),
+        }
+      )
+    if self.dev_docs is not None:
+      ret.update(
+        {
+          "optim_seq_dev": torch.cat(
+            [self.wrapped_prompt[0], self.dev_docs[idx]], dim=-1
+          ),
+          "orig_seq_dev": torch.cat(
+            [self.orig_wrapped_prompt[0], self.dev_docs[idx]], dim=-1
+          ),
+        }
+      )
+
+    return ret
 
 
 # %%
@@ -718,7 +771,6 @@ def optim_gcg(
   n_epochs: int,
   kl_every: int,
   log_fpath: str,
-  id_save_fpath: str,
   batch_size: int = 10,
   top_k: int = 256,
   gamma: float = 0.0,
@@ -774,14 +826,12 @@ def optim_gcg(
     if suffix_mode and loss < best_loss:
       best_ids = ids
       best_loss = loss
-      torch.save(best_ids, id_save_fpath)
     elif not suffix_mode and i % kl_every == 0:
       cur_kl, cur_std = compute_dataset_kl(model, dataset, batch_size=batch_size)
       if cur_kl < best_kl:
         best_ids = ids
         best_kl = cur_kl
         best_std = cur_std
-        torch.save(best_ids, id_save_fpath)
 
     best_loss = min(loss, best_loss)
 
@@ -794,10 +844,22 @@ def optim_gcg(
         "best_std": best_std,
         "cur_kl": cur_kl if cur_kl is not None else best_kl,
         "cur_std": cur_std if cur_std is not None else best_std,
-        "orig_prompt": tokenizer.decode(
-          dataset.orig_wrapped_prompt[0, dataset.orig_prompt_slice]
-        ),
-        "prompt": tokenizer.decode(ids[0]),
+        "orig_prompt": {
+          "text": tokenizer.decode(dataset.orig_wrapped_prompt[0]),
+          "ids": dataset.orig_wrapped_prompt[0].tolist(),
+          "prompt_start_slice": dataset.orig_prompt_slice.start,
+          "prompt_end_slice": dataset.orig_prompt_slice.stop,
+          "doc_start_slice": dataset.orig_doc_slice.start,
+          "doc_end_slice": dataset.orig_doc_slice.stop,
+        },
+        "optim_prompt": {
+          "text": tokenizer.decode(dataset.wrapped_prompt[0]),
+          "ids": dataset.wrapped_prompt[0].tolist(),
+          "prompt_start_slice": dataset.prompt_slice.start,
+          "prompt_end_slice": dataset.prompt_slice.stop,
+          "doc_start_slice": dataset.doc_slice.start,
+          "doc_end_slice": dataset.doc_slice.stop,
+        },
         "nll_prompt": -log_prob_prompt,
       }
     )
